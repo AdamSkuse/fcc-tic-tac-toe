1424800fda424fe6eb267a28ea54f629b8835dd5
AdamSkuse <a.t.skuse@gmail.com>
2017-06-06 18:17:06 -0700

    Refactor creates refreshTotalsArray();

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -48,16 +48,7 @@ function checkForWinner(index, playerX) {
         playerNumber = 5;
     }
     movesLog[index] = playerNumber;
-    totalsArray = [];
-    totalsArray.push(movesLog[0] + movesLog[1] + movesLog[2]);
-    totalsArray.push(movesLog[3] + movesLog[4] + movesLog[5]);
-    totalsArray.push(movesLog[6] + movesLog[7] + movesLog[8]);
-    totalsArray.push(movesLog[0] + movesLog[3] + movesLog[6]);
-    totalsArray.push(movesLog[1] + movesLog[4] + movesLog[7]);
-    totalsArray.push(movesLog[2] + movesLog[5] + movesLog[8]);
-    totalsArray.push(movesLog[0] + movesLog[4] + movesLog[8]);
-    totalsArray.push(movesLog[2] + movesLog[4] + movesLog[6]);
-
+    refreshTotalsArray();
     if (totalsArray.includes(3)) {
         var winMessageDiv = document.getElementById("win-message-div");
         var p = document.createElement("p");
@@ -87,4 +78,22 @@ function checkForDraw() {
         p.appendChild(winMessageText);
         winMessageDiv.appendChild(p);
    }
+}
+
+function refreshTotalsArray() {
+    totalsArray = [];
+    totalsArray.push(movesLog[0] + movesLog[1] + movesLog[2]); // top row
+    totalsArray.push(movesLog[3] + movesLog[4] + movesLog[5]); // middle row
+    totalsArray.push(movesLog[6] + movesLog[7] + movesLog[8]); // bottom row
+    totalsArray.push(movesLog[0] + movesLog[3] + movesLog[6]); // first column
+    totalsArray.push(movesLog[1] + movesLog[4] + movesLog[7]); // second column
+    totalsArray.push(movesLog[2] + movesLog[5] + movesLog[8]); // third column
+    totalsArray.push(movesLog[0] + movesLog[4] + movesLog[8]); // diagonal: top-left to bottom-right
+    totalsArray.push(movesLog[2] + movesLog[4] + movesLog[6]); // diagonal: top-right to bottom-left
+}
+
+function computerMove() {
+// computer is '0s'
+
+  
 }
